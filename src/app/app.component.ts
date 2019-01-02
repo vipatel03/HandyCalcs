1aaf9b3dee9b5511fc741d461cc9cb47332844a8
vipatel03 <vipatel03@gmail.com>
2019-01-02 07:01:28 -0800

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,21 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { Component, OnInit } from '@angular/core';
-import { Car } from './domain/car';
-import { CarService } from './services/carservice';
-
-export class PrimeCar implements Car {
-    constructor(public vin?, public year?, public brand?, public color?) {}
-}
+import { AnalyticsService } from './@core/utils/analytics.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [CarService]
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
 
-    displayDialog: boolean;
+  constructor(private analytics: AnalyticsService) {
+  }
 
-    car: Car = new PrimeCar();
-
-    selectedCar: Car;
-
-    newCar: boolean;
-
-    cars: Car[];
-
-    cols: any[];
-
-    constructor(private carService: CarService) { }
-
-    ngOnInit() {
-        this.carService.getCarsSmall().then(cars => this.cars = cars);
-
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
-    }
-
-    showDialogToAdd() {
-        this.newCar = true;
-        this.car = new PrimeCar();
-        this.displayDialog = true;
-    }
-
-    save() {
-        const cars = [...this.cars];
-        if (this.newCar) {
-            cars.push(this.car);
-        } else {
-            cars[this.findSelectedCarIndex()] = this.car;
-        }
-        this.cars = cars;
-        this.car = null;
-        this.displayDialog = false;
-    }
-
-    delete() {
-        const index = this.findSelectedCarIndex();
-        this.cars = this.cars.filter((val, i) => i !== index);
-        this.car = null;
-        this.displayDialog = false;
-    }
-
-    onRowSelect(event) {
-        this.newCar = false;
-        this.car = {...event.data};
-        this.displayDialog = true;
-    }
-
-    findSelectedCarIndex(): number {
-        return this.cars.indexOf(this.selectedCar);
-    }
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
+  }
 }
